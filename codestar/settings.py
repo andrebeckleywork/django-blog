c9c73e7b0b0245a9870e1b96543aef91c6a27f8d
Andre Beckly <andre.beckley@gmail.com>
2025-03-31 17:57:26 +0100

    added herokuapp stuff

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n0c_!-1#7rudmqs7lkohf=)&7(-b&(-&4+zq+14b)b40!bv)op'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
